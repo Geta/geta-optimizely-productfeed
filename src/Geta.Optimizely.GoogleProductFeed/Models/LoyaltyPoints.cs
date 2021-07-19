149e7242a13a9e8e6d3e2f9a0f43feacb39fb8b7
Māris Krivtežs <marisks@gmail.com>
2021-07-19 13:32:55 +0300

    Added new fields from old repo.

---
+++ src/Geta.Optimizely.GoogleProductFeed/Models/LoyaltyPoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace Geta.Optimizely.GoogleProductFeed.Models
+{
+    public class LoyaltyPoints
+    {
+        [XmlElement("name", Namespace = "http://base.google.com/ns/1.0")]
+        public string Name { get; set; }
+
+        [XmlElement("points_value", Namespace = "http://base.google.com/ns/1.0")]
+        public int PointsValue { get; set; }
+
+        [XmlElement("ratio", Namespace = "http://base.google.com/ns/1.0")]
+        public double Ration { get; set; }
+    }
+}
