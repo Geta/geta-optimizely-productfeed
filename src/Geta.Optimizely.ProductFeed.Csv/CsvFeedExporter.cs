f0721024ce4ab4944b5165240b19a5759cf32b7f
Drew Kriger <akriger@womackelectric.com>
2025-08-28 11:05:10 -0400

    Refactor SerializeEntry to handle collections in CsvFeedExporter

--- src/Geta.Optimizely.ProductFeed.Csv/CsvFeedExporter.cs
+++ src/Geta.Optimizely.ProductFeed.Csv/CsvFeedExporter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Geta Digital. All rights reserved.
 // Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -30,8 +31,17 @@ public class CsvFeedExporter<TEntity>(CsvFeedDescriptor<TEntity> descriptor) : A
 
     public override byte[] SerializeEntry(object value, CancellationToken cancellationToken)
     {
-        _writer.WriteRecord(value);
-        _writer.NextRecord();
+        // Handle collections (e.g., IEnumerable<CsvEntry>) by writing all records.
+        // Use non-generic IEnumerable to avoid generic invariance issues.
+        if (value is IEnumerable enumerable && value is not string)
+        {
+            _writer.WriteRecords(enumerable);
+        }
+        else
+        {
+            _writer.WriteRecord(value);
+            _writer.NextRecord();
+        }
 
         return [];
     }
